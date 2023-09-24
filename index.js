d1d5261d3c32ad41f6982257cdc9b9be7716749d
mahmeddismail <109833761+mahmeddismail@users.noreply.github.com>
2023-09-24 14:28:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,152 +1,43 @@
-let Quotes = [
-  [["Frankly, my dear, I don't give a damn."], ["--Gone with the Wind"]],
-  [["Here's looking at you, kid."], ["--Casablanca"]],
-  [["May the Force be with you."], ["--Star Wars"]],
-  [["I'm the king of the world!"], ["--Titanic"]],
-  [["I'll be back."], ["--The Terminator"]],
-  [["There's no place like home."], ["--The Wizard of Oz"]],
-  [["I see dead people."], ["--The Sixth Sense"]],
-  [["Houston, we have a problem."], ["--Apollo 13"]],
-  [["Why so serious?"], ["--The Dark Knight"]],
-  [["You can't handle the truth!"], ["--A Few Good Men"]],
-  [["I'm going to make him an offer he can't refuse."], ["--The Godfather"]],
-  [["Hasta la vista, baby."], ["--Terminator 2: Judgment Day"]],
-  [["You talking to me?"], ["--Taxi Driver"]],
-  [["I love the smell of napalm in the morning."], ["--Apocalypse Now"]],
-  [["Keep your friends close, but your enemies closer."], ["--The Godfather Part II"]],
-  [["I'll have what she's having."], ["--When Harry Met Sally"]],
-  [["You can't sit with us!"], ["--Mean Girls"]],
-  [["Why don't you make like a tree and get outta here?"], ["--Back to the Future"]],
-  [["I'm just a girl, standing in front of a boy, asking him to love her."], ["--Notting Hill"]],
-  [["You had me at hello."], ["--Jerry Maguire"]],
-  [["To infinity and beyond!"], ["--Toy Story"]],
-  [["I'm the Dude. So that's what you call me."], ["--The Big Lebowski"]],
-  [["Life is like a box of chocolates; you never know what you're gonna get."], ["--Forrest Gump"]],
-  [["There's no crying in baseball!"], ["--A League of Their Own"]],
-  [["I'll get you, my pretty, and your little dog too!"], ["--The Wizard of Oz"]],
-  [["I volunteer as tribute."], ["--The Hunger Games"]],
-  [["I'm king of the world!"], ["--Titanic"]],
-  [["I'm too old for this shit."], ["--Lethal Weapon"]],
-  [["I feel the need... the need for speed."], ["--Top Gun"]],
-  [["Here's Johnny!"], ["--The Shining"]],
-  [["Yo, Adrian!"], ["--Rocky"]],
-  [["Say hello to my little friend!"], ["--Scarface"]],
-  [["You can't handle the truth!"], ["--A Few Good Men"]],
-  [["There's no crying in baseball!"], ["--A League of Their Own"]],
-  [["I'm the king of the world!"], ["--Titanic"]],
-  [["I'll be back."], ["--The Terminator"]],
-  [["I see dead people."], ["--The Sixth Sense"]],
-  [["Houston, we have a problem."], ["--Apollo 13"]],
-  [["Why so serious?"], ["--The Dark Knight"]],
-  [["You can't handle the truth!"], ["--A Few Good Men"]],
-  [["I'm going to make him an offer he can't refuse."], ["--The Godfather"]],
-  [["Hasta la vista, baby."], ["--Terminator 2: Judgment Day"]],
-  [["You talking to me?"], ["--Taxi Driver"]],
-  [["I love the smell of napalm in the morning."], ["--Apocalypse Now"]],
-  [["Keep your friends close, but your enemies closer."], ["--The Godfather Part II"]],
-  [["I'll have what she's having."], ["--When Harry Met Sally"]],
-  [["You can't sit with us!"], ["--Mean Girls"]],
-  [["Why don't you make like a tree and get outta here?"], ["--Back to the Future"]],
-  [["I'm just a girl, standing in front of a boy, asking him to love her."], ["--Notting Hill"]],
-  [["You had me at hello."], ["--Jerry Maguire"]],
-  [["To infinity and beyond!"], ["--Toy Story"]],
-  [["I'm the Dude. So that's what you call me."], ["--The Big Lebowski"]],
-  [["Life is like a box of chocolates; you never know what you're gonna get."], ["--Forrest Gump"]],
-  [["There's no crying in baseball!"], ["--A League of Their Own"]],
-  [["I'll get you, my pretty, and your little dog too!"], ["--The Wizard of Oz"]],
-  [["I volunteer astribute."], ["--The Hunger Games"]],
-  [["I'm king of the world!"], ["--Titanic"]],
-  [["I'm too old for this shit."], ["--Lethal Weapon"]],
-  [["I feel the need... the need for speed."], ["--Top Gun"]],
-  [["Here's Johnny!"], ["--The Shining"]],
-  [["Yo, Adrian!"], ["--Rocky"]],
-  [["Say hello to my little friend!"], ["--Scarface"]],
-  [["I'm the king of the world!"], ["--Titanic"]],
-  [["I'll be back."], ["--The Terminator"]],
-  [["I see dead people."], ["--The Sixth Sense"]],
-  [["Houston, we have a problem."], ["--Apollo 13"]],
-  [["Why so serious?"], ["--The Dark Knight"]],
-  [["You can't handle the truth!"], ["--A Few Good Men"]],
-  [["I'm going to make him an offer he can't refuse."], ["--The Godfather"]],
-  [["Hasta la vista, baby."], ["--Terminator 2: Judgment Day"]],
-  [["You talking to me?"], ["--Taxi Driver"]],
-  [["I love the smell of napalm in the morning."], ["--Apocalypse Now"]],
-  [["Keep your friends close, but your enemies closer."], ["--The Godfather Part II"]],
-  [["I'll have what she's having."], ["--When Harry Met Sally"]],
-  [["You can't sit with us!"], ["--Mean Girls"]],
-  [["Why don't you make like a tree and get outta here?"], ["--Back to the Future"]],
-  [["I'm just a girl, standing in front of a boy, asking him to love her."], ["--Notting Hill"]],
-  [["You had me at hello."], ["--Jerry Maguire"]],
-  [["To infinity and beyond!"], ["--Toy Story"]],
-  [["I'm the Dude. So that's what you call me."], ["--The Big Lebowski"]],
-  [["Life is like a box of chocolates; you never know what you're gonna get."], ["--Forrest Gump"]],
-  [["There's no crying in baseball!"], ["--A League of Their Own"]],
-  [["I'll get you, my pretty, and your little dog too!"], ["--The Wizard of Oz"]],
-  [["I volunteer as tribute."], ["--The Hunger Games"]],
-  [["I'm king of the world!"], ["--Titanic"]],
-  [["I'm too old for this shit."], ["--Lethal Weapon"]],
-  [["I feel the need... the need for speed."], ["--Top Gun"]],
-  [["Here's Johnny!"], ["--The Shining"]],
-  [["Yo, Adrian!"], ["--Rocky"]],
-  [["Say hello to my little friend!"], ["--Scarface"]],
-  [["You can't handle the truth!"], ["--A Few Good Men"]],
-  [["There's no crying in baseball!"], ["--A League of Their Own"]],
-  [["I'm the king of the world!"], ["--Titanic"]],
-  [["I'll be back."], ["--The Terminator"]],
-  [["I see dead people."], ["--The Sixth Sense"]],
-  [["Houston, we have a problem."], ["--Apollo 13"]],
-  [["Why so serious?"], ["--The Dark Knight"]],
-  [["You can't handle the truth!"], ["--A Few Good Men"]],
-  [["I'm going to make him an offer he can't refuse."], ["--The Godfather"]],
-  [["Hasta la vista, baby."], ["--Terminator 2: Judgment Day"]],
-  [["You talking to me?"], ["--Taxi Driver"]],
-  [["I love the smell of napalm in the morning."], ["--Apocalypse Now"]],
-  [["Keep your friends close, but your enemies closer."], ["--The Godfather Part II"]],
-  [["I'll have what she's having."], ["--When Harry Met Sally"]],
-  [["You can't sit with us!"], ["--Mean Girls"]],
-  [["Why don't you make like a tree and get outta here?"], ["--Back to the Future"]],
-  [["I'm just a girl, standing in front of a boy, asking him to love her."], ["--Notting Hill"]],
-  [["You had me at hello."], ["--Jerry Maguire"]],
-  [["To infinity and beyond!"], ["--Toy Story"]],
-  [["I'm the Dude. So that's what you call me."], ["--The Big Lebowski"]],
-  [["Life is like a box of chocolates; you never know what you're gonna get."], ["--Forrest Gump"]],
-  [["There's no crying in baseball!"], ["--A League of Their Own"]],
-  [["I'll get you, my pretty, and your little dog too!"], ["--The Wizard of OZ"]]
-]
-let myBtn=document.querySelector('.myBtn')
-let myBody=document.querySelector('body')
+let Quotes = fetch('Quotes.json')
+  .then((response) => response.json())
+  .then((data) => { Quotes = data })
+  .catch(error => { console.log('Error :', error); });
+let myBtn = document.querySelector('.myBtn')
+let myBody = document.querySelector('body')
 
 
 
-var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF', '#FF6600', '#FFCC33', '#FF3366', '#FFFF00', '#00B399',
+  '#E6B366', '#3333E6', '#999900', '#99FF66', '#4D4DB3',
+  '#80B333', '#809933', '#E6B3FF', '#6680FF', '#669900',];
 
 
-let displayedQuote=null;
+let displayedQuote = null;
 function Quote() {
   let value = Math.floor(Math.random() * Quotes.length);
-  while(displayedQuote==Quotes[value]){
-  value = Math.floor(Math.random() * Quotes.length);
+  while (displayedQuote == Quotes[value]) {
+    value = Math.floor(Math.random() * Quotes.length);
 
   }
-  let items = Quotes[value][0];
-  let author = Quotes[value][1];
-  console.log(items);
+  let myQuote = Quotes[value].quote;
+  let movie = Quotes[value].movie;
+  console.log(myQuote);
 
-  // console.log(items + " " + author);
-  document.querySelector(".Quote").innerHTML = items;
-  document.querySelector(".author").innerHTML = author;
-  displayedQuote=Quotes[value]
+  // console.log(myQuote + " " + author);
+  document.querySelector(".Quote").innerHTML = myQuote;
+  document.querySelector(".movie").innerHTML = movie;
+  displayedQuote = Quotes[value]
 }
-function changeColor(){
+function changeColor() {
   let randomColor = colors[Math.floor(Math.random() * colors.length)];
   myBody.style.backgroundColor = randomColor;
 }
@@ -155,4 +46,4 @@ myBtn.addEventListener("click", function () {
   Quote()
   changeColor()
 
-  });
+});
