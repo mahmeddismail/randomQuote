b5b5f1fbe60dbd8fbbb74f1e64a16c8bc08154c3
mahmeddismail <109833761+mahmeddismail@users.noreply.github.com>
2023-09-26 14:13:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,118 @@
-let Quotes = fetch('Quotes.json')
-  .then((response) => response.json())
-  .then((data) => { Quotes = data })
-  .catch(error => { console.log('Error :', error); });
+let Quotes = [
+  [["Frankly, my dear, I don't give a damn."], ["--Gone with the Wind"]],
+  [["Here's looking at you, kid."], ["--Casablanca"]],
+  [["May the Force be with you."], ["--Star Wars"]],
+  [["I'm the king of the world!"], ["--Titanic"]],
+  [["I'll be back."], ["--The Terminator"]],
+  [["There's no place like home."], ["--The Wizard of Oz"]],
+  [["I see dead people."], ["--The Sixth Sense"]],
+  [["Houston, we have a problem."], ["--Apollo 13"]],
+  [["Why so serious?"], ["--The Dark Knight"]],
+  [["You can't handle the truth!"], ["--A Few Good Men"]],
+  [["I'm going to make him an offer he can't refuse."], ["--The Godfather"]],
+  [["Hasta la vista, baby."], ["--Terminator 2: Judgment Day"]],
+  [["You talking to me?"], ["--Taxi Driver"]],
+  [["I love the smell of napalm in the morning."], ["--Apocalypse Now"]],
+  [["Keep your friends close, but your enemies closer."], ["--The Godfather Part II"]],
+  [["I'll have what she's having."], ["--When Harry Met Sally"]],
+  [["You can't sit with us!"], ["--Mean Girls"]],
+  [["Why don't you make like a tree and get outta here?"], ["--Back to the Future"]],
+  [["I'm just a girl, standing in front of a boy, asking him to love her."], ["--Notting Hill"]],
+  [["You had me at hello."], ["--Jerry Maguire"]],
+  [["To infinity and beyond!"], ["--Toy Story"]],
+  [["I'm the Dude. So that's what you call me."], ["--The Big Lebowski"]],
+  [["Life is like a box of chocolates; you never know what you're gonna get."], ["--Forrest Gump"]],
+  [["There's no crying in baseball!"], ["--A League of Their Own"]],
+  [["I'll get you, my pretty, and your little dog too!"], ["--The Wizard of Oz"]],
+  [["I volunteer as tribute."], ["--The Hunger Games"]],
+  [["I'm king of the world!"], ["--Titanic"]],
+  [["I'm too old for this shit."], ["--Lethal Weapon"]],
+  [["I feel the need... the need for speed."], ["--Top Gun"]],
+  [["Here's Johnny!"], ["--The Shining"]],
+  [["Yo, Adrian!"], ["--Rocky"]],
+  [["Say hello to my little friend!"], ["--Scarface"]],
+  [["You can't handle the truth!"], ["--A Few Good Men"]],
+  [["There's no crying in baseball!"], ["--A League of Their Own"]],
+  [["I'm the king of the world!"], ["--Titanic"]],
+  [["I'll be back."], ["--The Terminator"]],
+  [["I see dead people."], ["--The Sixth Sense"]],
+  [["Houston, we have a problem."], ["--Apollo 13"]],
+  [["Why so serious?"], ["--The Dark Knight"]],
+  [["You can't handle the truth!"], ["--A Few Good Men"]],
+  [["I'm going to make him an offer he can't refuse."], ["--The Godfather"]],
+  [["Hasta la vista, baby."], ["--Terminator 2: Judgment Day"]],
+  [["You talking to me?"], ["--Taxi Driver"]],
+  [["I love the smell of napalm in the morning."], ["--Apocalypse Now"]],
+  [["Keep your friends close, but your enemies closer."], ["--The Godfather Part II"]],
+  [["I'll have what she's having."], ["--When Harry Met Sally"]],
+  [["You can't sit with us!"], ["--Mean Girls"]],
+  [["Why don't you make like a tree and get outta here?"], ["--Back to the Future"]],
+  [["I'm just a girl, standing in front of a boy, asking him to love her."], ["--Notting Hill"]],
+  [["You had me at hello."], ["--Jerry Maguire"]],
+  [["To infinity and beyond!"], ["--Toy Story"]],
+  [["I'm the Dude. So that's what you call me."], ["--The Big Lebowski"]],
+  [["Life is like a box of chocolates; you never know what you're gonna get."], ["--Forrest Gump"]],
+  [["There's no crying in baseball!"], ["--A League of Their Own"]],
+  [["I'll get you, my pretty, and your little dog too!"], ["--The Wizard of Oz"]],
+  [["I volunteer astribute."], ["--The Hunger Games"]],
+  [["I'm king of the world!"], ["--Titanic"]],
+  [["I'm too old for this shit."], ["--Lethal Weapon"]],
+  [["I feel the need... the need for speed."], ["--Top Gun"]],
+  [["Here's Johnny!"], ["--The Shining"]],
+  [["Yo, Adrian!"], ["--Rocky"]],
+  [["Say hello to my little friend!"], ["--Scarface"]],
+  [["I'm the king of the world!"], ["--Titanic"]],
+  [["I'll be back."], ["--The Terminator"]],
+  [["I see dead people."], ["--The Sixth Sense"]],
+  [["Houston, we have a problem."], ["--Apollo 13"]],
+  [["Why so serious?"], ["--The Dark Knight"]],
+  [["You can't handle the truth!"], ["--A Few Good Men"]],
+  [["I'm going to make him an offer he can't refuse."], ["--The Godfather"]],
+  [["Hasta la vista, baby."], ["--Terminator 2: Judgment Day"]],
+  [["You talking to me?"], ["--Taxi Driver"]],
+  [["I love the smell of napalm in the morning."], ["--Apocalypse Now"]],
+  [["Keep your friends close, but your enemies closer."], ["--The Godfather Part II"]],
+  [["I'll have what she's having."], ["--When Harry Met Sally"]],
+  [["You can't sit with us!"], ["--Mean Girls"]],
+  [["Why don't you make like a tree and get outta here?"], ["--Back to the Future"]],
+  [["I'm just a girl, standing in front of a boy, asking him to love her."], ["--Notting Hill"]],
+  [["You had me at hello."], ["--Jerry Maguire"]],
+  [["To infinity and beyond!"], ["--Toy Story"]],
+  [["I'm the Dude. So that's what you call me."], ["--The Big Lebowski"]],
+  [["Life is like a box of chocolates; you never know what you're gonna get."], ["--Forrest Gump"]],
+  [["There's no crying in baseball!"], ["--A League of Their Own"]],
+  [["I'll get you, my pretty, and your little dog too!"], ["--The Wizard of Oz"]],
+  [["I volunteer as tribute."], ["--The Hunger Games"]],
+  [["I'm king of the world!"], ["--Titanic"]],
+  [["I'm too old for this shit."], ["--Lethal Weapon"]],
+  [["I feel the need... the need for speed."], ["--Top Gun"]],
+  [["Here's Johnny!"], ["--The Shining"]],
+  [["Yo, Adrian!"], ["--Rocky"]],
+  [["Say hello to my little friend!"], ["--Scarface"]],
+  [["You can't handle the truth!"], ["--A Few Good Men"]],
+  [["There's no crying in baseball!"], ["--A League of Their Own"]],
+  [["I'm the king of the world!"], ["--Titanic"]],
+  [["I'll be back."], ["--The Terminator"]],
+  [["I see dead people."], ["--The Sixth Sense"]],
+  [["Houston, we have a problem."], ["--Apollo 13"]],
+  [["Why so serious?"], ["--The Dark Knight"]],
+  [["You can't handle the truth!"], ["--A Few Good Men"]],
+  [["I'm going to make him an offer he can't refuse."], ["--The Godfather"]],
+  [["Hasta la vista, baby."], ["--Terminator 2: Judgment Day"]],
+  [["You talking to me?"], ["--Taxi Driver"]],
+  [["I love the smell of napalm in the morning."], ["--Apocalypse Now"]],
+  [["Keep your friends close, but your enemies closer."], ["--The Godfather Part II"]],
+  [["I'll have what she's having."], ["--When Harry Met Sally"]],
+  [["You can't sit with us!"], ["--Mean Girls"]],
+  [["Why don't you make like a tree and get outta here?"], ["--Back to the Future"]],
+  [["I'm just a girl, standing in front of a boy, asking him to love her."], ["--Notting Hill"]],
+  [["You had me at hello."], ["--Jerry Maguire"]],
+  [["To infinity and beyond!"], ["--Toy Story"]],
+  [["I'm the Dude. So that's what you call me."], ["--The Big Lebowski"]],
+  [["Life is like a box of chocolates; you never know what you're gonna get."], ["--Forrest Gump"]],
+  [["There's no crying in baseball!"], ["--A League of Their Own"]],
+  [["I'll get you, my pretty, and your little dog too!"], ["--The Wizard of OZ"]]
+]
 let myBtn = document.querySelector('.myBtn')
 let myBody = document.querySelector('body')
 
@@ -28,13 +139,13 @@ function Quote() {
     value = Math.floor(Math.random() * Quotes.length);
 
   }
-  let myQuote = Quotes[value].quote;
-  let movie = Quotes[value].movie;
-  console.log(myQuote);
+  let items = Quotes[value][0];
+  let author = Quotes[value][1];
+  console.log(items);
 
-  // console.log(myQuote + " " + author);
-  document.querySelector(".Quote").innerHTML = myQuote;
-  document.querySelector(".movie").innerHTML = movie;
+  // console.log(items + " " + author);
+  document.querySelector(".Quote").innerHTML = items;
+  document.querySelector(".author").innerHTML = author;
   displayedQuote = Quotes[value]
 }
 function changeColor() {
